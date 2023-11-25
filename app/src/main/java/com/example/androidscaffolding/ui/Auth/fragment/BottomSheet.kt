9603774d98336a98b54e376fbebdff41576fd477
lhj257 <hojin159753@naver.com>
2023-11-25 18:27:56 +0900

    231125 localtodo bottomsheet 확인

--- app/src/main/java/com/example/androidscaffolding/ui/Auth/fragment/BottomSheet.kt
+++ app/src/main/java/com/example/androidscaffolding/ui/Auth/fragment/BottomSheet.kt
@@ -1,13 +1,10 @@
 package com.example.androidscaffolding.ui.Auth.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.androidscaffolding.R
 import com.example.androidscaffolding.databinding.FragmentBottomSheetBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheet : BottomSheetDialogFragment() {
