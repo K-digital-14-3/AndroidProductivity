0bb3160612c379bddb8336a7a3783cdf20f0161e
lhj257 <hojin159753@naver.com>
2023-11-25 18:23:31 +0900

    231125 localtodo bottomsheet 확인

--- app/src/main/java/com/example/androidscaffolding/ui/Auth/LocalTodoFragment.kt
+++ app/src/main/java/com/example/androidscaffolding/ui/Auth/LocalTodoFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.androidscaffolding.R
 import com.example.androidscaffolding.databinding.FragmentLocalTodoBinding
 import com.example.androidscaffolding.ui.Auth.adapter.recycleradapter
 import com.example.androidscaffolding.ui.Auth.fragment.BottomSheet
@@ -15,11 +12,11 @@ import com.example.androidscaffolding.ui.Auth.fragment.BottomSheet
 
 class LocalTodoFragment : Fragment() {
     lateinit var binding: FragmentLocalTodoBinding
-    // TODO: Rename and change types of parameters
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        OpeningDialogFragment().show(childFragmentManager, "GAME_DIALOG")
     }
 
     override fun onCreateView(
@@ -27,16 +24,24 @@ class LocalTodoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLocalTodoBinding.inflate(layoutInflater, container, false)
-        return binding.root
+
+        binding.newTaskBtn.setOnClickListener {
+            BottomSheet().show(childFragmentManager,BottomSheet().tag)
+        }
+
 
         val datas= mutableListOf<String>()
 
         binding.taskrecycler.adapter=recycleradapter(datas)
+        return binding.root
 
 
-        binding.newTaskBtn.setOnClickListener {
-            BottomSheet().show(childFragmentManager,"newTask")
-        }
+
+
+
+
+
+
     }
 
 
